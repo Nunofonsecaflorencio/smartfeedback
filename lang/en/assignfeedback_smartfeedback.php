296c1eed200a46e0ba28694721b2c0db04d79288
Nuno Fonseca Florencio <nunofonsecaflorencio@gmail.com>
2025-04-25 16:56:55 +0200

    starter code

---
+++ lang/en/assignfeedback_smartfeedback.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Strings for component 'assignfeedback_smartfeedback', language 'en'
+ *
+ * @package   assignfeedback_smartfeedback
+ * @copyright 2025, Nuno Fonseca <nunofonsecaflorencio@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['default'] = 'Enabled by default';
+$string['default_help'] = 'If set, this feedback method will be enabled by default for all new assignments.';
+$string['enabled'] = 'Smart feedback';
+$string['enabled_help'] = 'If enabled, this will provide AI-powered automatic feedback for student submissions.';
+$string['pluginname'] = 'Smart Feedback';
